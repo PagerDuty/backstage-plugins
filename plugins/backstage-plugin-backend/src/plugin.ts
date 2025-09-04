0c17302f3aa1cf11c03ede1e874c2ab61ee3d6b0
Jordan Kueh <jkueh@users.noreply.github.com>
2025-09-04 14:47:19 +0930

    feat: Allow default Backstage auth policy on plugin route

--- plugins/backstage-plugin-backend/src/plugin.ts
+++ plugins/backstage-plugin-backend/src/plugin.ts
@@ -12,7 +12,7 @@ class CatalogFetchApi {
   constructor(
     private readonly logger: LoggerService,
     private readonly auth: AuthService,
-  ) {}
+  ) { }
 
   async fetch(
     input: unknown,
@@ -62,10 +62,17 @@ export const pagerDutyPlugin = createBackendPlugin({
             }),
           }),
         );
-        httpRouter.addAuthPolicy({
-          path: '/',
-          allow: 'unauthenticated',
-        });
+        // The default Backstage behaviour is to require authentication on routes.
+        // https://backstage.io/docs/backend-system/core-services/http-router/#using-the-service
+        // Setting disableUnauthenticatedAccess to true will revert this plugin's route authentication
+        // policy back to the default Backstage behaviour.
+        const disableUnauthenticatedAccess = config.getOptionalBoolean('pagerDuty.disableUnauthenticatedAccess') ?? false;
+        if (!disableUnauthenticatedAccess) {
+          httpRouter.addAuthPolicy({
+            path: '/',
+            allow: 'unauthenticated',
+          });
+        }
       },
     });
   },
